1c4f7dfe243b2297a1a63062beb0997969f6b373
Andrés Martínez <autummata@gmail.com>
2025-03-08 18:34:55 -0600

    Fix: IDs creation for Students

--- src/main/java/com/example/oci_microservice/model/Student.java
+++ src/main/java/com/example/oci_microservice/model/Student.java
@@ -1,8 +1,8 @@
 package com.example.oci_microservice.model;
 
-import com.example.utils.CustomIdGenerator;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
@@ -13,22 +13,21 @@ import lombok.Data;
 @Data
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long studentId;
+
+    @PrePersist
+    private void ensureId() {
+        if (this.studentId == null) {
+            this.studentId = (long) (10000 + Math.random() * 90000);
+        }
+    }
 
     /**
      * This method is called before the entity is persisted.
      * It ensures that the studentId is set to a randomly generated value
      * if it is not already assigned.
      */
-
-    @PrePersist
-    protected void onCreate() {
-        if (this.studentId == null) {
-            this.studentId = CustomIdGenerator.generateRandomId();
-        }
-    }
-
-
     private String firstName;
     private String lastName;
     private String email;
