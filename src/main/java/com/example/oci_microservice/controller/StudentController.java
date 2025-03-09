a341b42a2c86261f8653ceb732fe2c5588410b50
Andrés Martínez <autummata@gmail.com>
2025-03-08 18:40:04 -0600

    Add: RequestBody to Create Student

--- src/main/java/com/example/oci_microservice/controller/StudentController.java
+++ src/main/java/com/example/oci_microservice/controller/StudentController.java
@@ -18,6 +18,7 @@ package com.example.oci_microservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,8 +46,8 @@ public class StudentController {
         return "This (student) route is working! :3";
     }
 
-    @PostMapping("/")
-    public Student createStudent(Student student) {
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
         return studentService.createStudent(student);
     }
 
